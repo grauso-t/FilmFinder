68fc20ce8f98e161258eead87283998fbd280f25
grauso-t <grausoteodoro@gmail.com>
2025-06-13 11:07:46 +0200

    Edit documentation and db

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/')
-    DATABASE_NAME = os.getenv('DATABASE_NAME', 'filmfinder')
+    MONGODB_URI = os.getenv('MONGODB_URI')
+    DATABASE_NAME = os.getenv('DATABASE_NAME')
     DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
